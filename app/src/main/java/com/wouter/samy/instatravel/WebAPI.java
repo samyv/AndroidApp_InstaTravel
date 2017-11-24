c4e460ddd9f1ab1cf3fa46586177aea7b2c414b3
wouter.heerwegh <wouter.heerwegh@student.kuleuven.be>
2017-11-24 11:24:43 +0100

    FUCK YOU

---
+++ app/src/main/java/com/wouter/samy/instatravel/WebAPI.java
@@ -0,0 +1,41 @@
+package com.wouter.samy.instatravel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class WebAPI extends AppCompatActivity {
+    //pls kill me now
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_api);
+        final TextView mTextView = (TextView) findViewById(R.id.textView);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://api.a17-sd510.studev.groept.be/createRegister";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+}
