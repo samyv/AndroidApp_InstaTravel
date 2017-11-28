c016203fd534949836e4c1a4e14ecba02e0ee9e3
wouter.heerwegh <wouter.heerwegh@student.kuleuven.be>
2017-11-28 13:52:04 +0100

    camera begin

---
+++ app/src/main/java/com/wouter/samy/instatravel/Cameratab.java
@@ -0,0 +1,39 @@
+package com.wouter.samy.instatravel;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Cameratab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cameratab);
+        if(checkCameraHardware(this)){
+            getCameraInstance();
+        }
+    }
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+}
